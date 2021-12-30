53e538773c021305d44be51305c519733f28d2a3
withewolf-dev <itskashyap26@gmail.com>
2021-12-30 09:54:20 +0530

    done

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+const withTM = require("next-transpile-modules")([
+  "@mui/material",
+  "@mui/system",
+  "@mui/icons-material", // If @mui/icons-material is being used
+]);
+
+module.exports = withTM({
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@mui/styled-engine": "@mui/styled-engine-sc",
+    };
+    return config;
+  },
+});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { CssBaseline } from "@mui/material";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "../styles/globalstyles";
+import theme from "../theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
-
-export default MyApp
+export default MyApp;
